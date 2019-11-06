2897795bcc9758c7ceb09aee0c1c87f5d587fbe8
Nick Espig <nickespig@gmail.com>
2019-11-06 17:54:23 +0100

    Improve debugger view

--- nachtalb/useful_tools/browser/misc_anon_tools.py
+++ nachtalb/useful_tools/browser/misc_anon_tools.py
@@ -1,4 +1,4 @@
-from pkg_resources import get_distribution
+from plone import api
 from zope.interface import implements
 
 from nachtalb.useful_tools.browser.useful_tools import UsefulToolsView
@@ -13,6 +13,15 @@ class MiscAnonToolsView(UsefulToolsView):
 
         https://github.com/4teamwork/opengever.maintenance/blob/6d33009b189472fd912e5310eee18de089957f0c/opengever/maintenance/browser/configure.zcml#L86-L91
         """
-        import pdb; pdb.set_trace()
+        portal = api.portal.get()
+        user = api.user.get_current()
+        catalog = api.portal.get_tool('portal_catalog')
+
+        self.context = self.get_non_ut_context()
+        ppath = self.context.getPhysicalPath()
+        path = '/'.join(ppath)
+        brain = next(iter(catalog(path={'query': path, 'depth': 0})), None)
+
+        __import__('pdb').set_trace()
         pass
         return "Done"
